59f61cfffd674bf6d06f926e1c399e796d4c10bb
ittaihu <ittaihu@gmail.com>
2018-07-31 13:40:28 +0300

    Working. Parameterized job.

--- createFileSpec.py
+++ createFileSpec.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import json
+import sys
 
 # for real life  - load the data, proces it and then dump. (So here its opposite because just playing).
 
@@ -16,8 +17,8 @@ with open("data_file.json", "w") as write_file:
 	json.dump(data, write_file, indent=4)
 json_string1 = json.dumps(data)
 json_string2 = json.dumps(data, indent=4)
-print 'string 1 = ' + json_string1
-print 'string 2 = ' + json_string2
+   #print 'string 1 = ' + json_string1
+   #print 'string 2 = ' + json_string2
 
 # load from file
 with open("data_file.json", "r") as read_file:
@@ -38,9 +39,14 @@ json_string = """
 """
 data2 = json.loads(json_string)
 
+# get data from input arguments
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+print 'Argument 1 = ', sys.argv[1]
+
 # process data
 print data
-data['files'][0]['target'] = "./testing4/"
+data['files'][0]['target'] = "./" + sys.argv[1] + "/"
 print data
 
 # dump again
